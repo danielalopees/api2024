3b71ed1f2c234ce92fa626d39818e7c06c967b62
Daniela Lopes <daniela.cristina7incloud@gmail.com>
2024-12-02 13:39:24 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Trash from "../src/assets/logo.svg";
 
 function App() {
+  const users = [];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <h1>Cadastro de um Usuarios</h1>
+        <input placeholder="Nome" name="nome" type="text" />
+        <input placeholder="Idade" name="idade" type="number" />
+        <input placeholder="E-mail" name="email" type="email" />
+        <button type="button">Cadastrar</button>
+      </form>
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>
+              Nome: <span>{user.name}</span>
+            </p>
+            <p>
+              Idade:<span> {user.age}</span>
+            </p>
+            <p>
+              Email:<span>5 {user.email}</span>
+            </p>
+          </div>
+          <button className="botao">
+            <img src={Trash}></img>
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
